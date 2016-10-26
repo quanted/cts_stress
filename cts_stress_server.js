8b0209171ffb8bd991f68f877bae20a00c7f4d04
popenc <i.nickpope@gmail.com>
2016-10-26 10:14:25 -0400

    express app.use adding /stress in front of static file paths. this will hopefully serve cts_stress's static files without having to explicit open them up on nginx.conf

--- cts_stress_server.js
+++ cts_stress_server.js
@@ -2,9 +2,13 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-// app.use('/static', express.static('js'));
-// app.use('/static', express.static('css'));
-app.use(express.static('public'));
+// below has following behavior locally vs production.
+// local: /css/cts_stress_styles.css, server: /stress/css/cts_stress_styles.css
+// app.use(express.static('public'));
+
+// will adding /stress to app.use fix this? or will it
+// make the production url /stress/stress/css/cts_stress_styles.css
+app.use('/stress', express.static('public'));
 
 app.get('/', function (req, res) {
   // res.send('Hello World!');
